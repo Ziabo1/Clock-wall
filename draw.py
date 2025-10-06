92e8c2448f1c69969e5391ee4a7519a2439263b7
Ziabo1 <122011146+Ziabo1@users.noreply.github.com>
2025-10-06 18:33:53 +0200

    Lavet kommentar for hver stykke kode

--- draw.py
+++ draw.py
@@ -2,37 +2,52 @@ import pygame
 import math
 from datetime import datetime
 
-
+# initialiser lys og pygame
 pygame.mixer.init()
 pygame.init()
+
+# Opretter et vindue i 640x480 pixels
 screen = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
+
+# Midtpunktet og radius for uret
 center_x, center_y = 325, 240
 radius = 230
+
+# Inlæser lyde for hver sekund og timeslag
 Clock_Noise = pygame.mixer.Sound("clock-ticking.wav")
 Hour_Noise = pygame.mixer.Sound("Hour-time-sound.wav")
+
+# Variabel til at holde styr på sidste sekund (for at undgå gentagelser)
 last_second = -600
 
+# Main funktion
 run_flag = True
 while run_flag is True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run_flag = False
 
+    # Baggrund farves hvid
     screen.fill((255, 255, 255))
 
+    # Tegner uret (grå cirkel) og centrum (blå prik)
     pygame.draw.circle(screen, (128, 128, 128), (325, 240), 230)
     pygame.draw.circle(screen, (15, 50, 128), (325, 240), 6)
 
+    # Hent aktuel tid
     now = datetime.now()
 
+    # Afspiller timeslag når klokken rammer en hel time
     if now.second == 0 and now.minute == 0:
         Hour_Noise.play()
 
+    # Afspil tikkelyd hvert sekund
     if now.second != last_second:
         Clock_Noise.play()
         last_second = now.second
 
+    # Tegner minus og skund markeringer for urskiven
     for i in range(60):
         angle = i * (2 * math.pi / 60)
         x = center_x + int(radius * math.cos(angle))
@@ -43,6 +58,7 @@ while run_flag is True:
         else:
             pygame.draw.rect(screen, (0, 255, 0), (x-2, y-2, 4, 4))
 
+    # Tegner sekundviseren
     s = datetime.now().second
     angle1 = 360 / 60 * s
     end_offset = [radius * math.cos(math.radians(angle1 - 90)),
@@ -51,6 +67,7 @@ while run_flag is True:
     pygame.draw.line(screen, (255, 0, 0),
                      (center_x, center_y), end_position, 3)
 
+    # Tegner minutviseren
     m = datetime.now().minute + datetime.now().second / 60
     angle1 = 360 / 60 * m
     end_offset = [radius * math.cos(math.radians(angle1 - 90)),
@@ -58,9 +75,10 @@ while run_flag is True:
     end_position = (center_x + end_offset[0], center_y + end_offset[1])
     pygame.draw.line(screen, (0, 0, 0), (center_x, center_y), end_position, 3)
 
+    # Tegn timeviseren (kortere og tykkere sort linje)
     h = datetime.now().hour % 12 + datetime.now().minute / 60
     angle1 = 360 / 12 * h
-    hour_length = radius - 20
+    hour_length = radius - 20  # Timeviser lidt kortere end de andre
     end_offset = [hour_length * math.cos(math.radians(angle1 - 90)),
                   hour_length * math.sin(math.radians(angle1 - 90))]
     end_position = (center_x + end_offset[0], center_y + end_offset[1])
